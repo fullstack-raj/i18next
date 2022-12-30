49081b7dd0b99aac01388dbdf89d5932edcfd0b2
Rajashekhar <adukani@celitotech.com>
2022-12-30 23:14:16 +0530

    internationalization

--- Desktop/PCFReact/LinearInputControl/src/landingpage/index.tsx
+++ Desktop/PCFReact/LinearInputControl/src/landingpage/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { FormattedMessage, IntlProvider, useIntl } from 'react-intl';
 import Test from '../components/Test';
+import systemValues from '../i18n/i18next';
 
 const LandingPage = () => {
 
@@ -9,14 +10,25 @@ const LandingPage = () => {
   const [translations, setTranslations] = React.useState<any>({});
   const [locale, setLocale] = React.useState('en');
 
+  // const compareValuesUpdates = (backendValues: any) => {
+  //   const result = { ...systemValues };
+  //   for (const key in backendValues) {
+  //     if (key in result) {
+  //       result[key] = backendValues[key];
+  //     } else {
+  //       result[key] = backendValues[key];
+  //     }
+  //   }
+  //   return result
+  // }
 
   React.useEffect(() => {
     fetch(`https://dummyjson.com/products`)
       .then((response) => response.json())
       .then((data) => {
+        // const result = compareValuesUpdates(data?.products[0]);
         debugger
-        const result = data?.products[0];
-        setTranslations(result);
+        setTranslations(data?.products[0]);
       });
   }, [locale]);
 
@@ -27,6 +39,8 @@ const LandingPage = () => {
       <h2>Type 1</h2>
       <FormattedMessage id="title" />
       <Test />
+      <h2>Type 4</h2>1
+      {systemValues.title}
     </IntlProvider>
 
   )
